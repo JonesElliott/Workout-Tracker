bd508f0c28faee72ff65353338db2fc7a2ff58c3
Elliott Jones <jones.elliott@outlook.com>
2020-10-27 16:29:19 -0700

    Remved console logs from front end

--- public/assets/js/workout.js
+++ public/assets/js/workout.js
@@ -1,7 +1,5 @@
 async function initWorkout() {
   const lastWorkout = await API.getLastWorkout();
-  console.log("Last workout: ", lastWorkout);
-  console.log("Total Duration: ", lastWorkout.totalDuration);
   if (lastWorkout) {
     document
       .querySelector("a[href='/exercise?']")
